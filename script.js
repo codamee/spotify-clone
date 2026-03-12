f4523d24a52375132215ddb71a0b7af8a77ab16b
naveen <185893220+codamee@users.noreply.github.com>
2026-03-12 11:56:48 +0530

    file:add songs.js

---
+++ script.js
@@ -0,0 +1,3 @@
+import { spotifyLibrary } from "./songs.js"
+let audio = new Audio(spotifyLibrary[2].url)
+// audio.play()
---
+++ songs.js
@@ -0,0 +1,92 @@
+export const spotifyLibrary = [
+    {
+        id: "1",
+        title: "Lost in the City",
+        artist: "Lofi Girl",
+        album: "Chilled Cow",
+        duration: "3:20",
+        cover: "https://picsum.photos/id/10/300/300",
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+    },
+    {
+        id: "2",
+        title: "Neon Dreams",
+        artist: "Synthwave Kid",
+        album: "Retro Future",
+        duration: "4:05",
+        cover: "https://picsum.photos/id/20/300/300",
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3"
+    },
+    {
+        id: "3",
+        title: "Mountain High",
+        artist: "Acoustic Soul",
+        album: "Nature's Path",
+        duration: "2:55",
+        cover: "https://picsum.photos/id/30/300/300",
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3"
+    },
+    {
+        id: "4",
+        title: "Midnight Jazz",
+        artist: "Blue Note Duo",
+        album: "Late Night Sessions",
+        duration: "5:12",
+        cover: "https://picsum.photos/id/40/300/300",
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3"
+    },
+    {
+        id: "5",
+        title: "Electric Pulse",
+        artist: "Digital Ghost",
+        album: "Code Runner",
+        duration: "3:45",
+        cover: "https://picsum.photos/id/50/300/300",
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3"
+    },
+    {
+        id: "6",
+        title: "Ocean Waves",
+        artist: "Zen Master",
+        album: "Meditation",
+        duration: "6:30",
+        cover: "https://picsum.photos/id/60/300/300",
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3"
+    },
+    {
+        id: "7",
+        title: "Golden Hour",
+        artist: "Summer Vibes",
+        album: "Beach Party",
+        duration: "3:15",
+        cover: "https://picsum.photos/id/70/300/300",
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-10.mp3"
+    },
+    {
+        id: "8",
+        title: "Cyberpunk",
+        artist: "Glitch Mob",
+        album: "System Failure",
+        duration: "4:22",
+        cover: "https://picsum.photos/id/80/300/300",
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-12.mp3"
+    },
+    {
+        id: "9",
+        title: "Rainy Sunday",
+        artist: "Piano Dreams",
+        album: "Soft Keys",
+        duration: "3:50",
+        cover: "https://picsum.photos/id/90/300/300",
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-14.mp3"
+    },
+    {
+        id: "10",
+        title: "Hard Rock Cafe",
+        artist: "The Strident",
+        album: "Electric Sky",
+        duration: "4:10",
+        cover: "https://picsum.photos/id/100/300/300",
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-16.mp3"
+    }
+];
