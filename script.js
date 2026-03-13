2608aa73d0c6795bba1125e870b1b7e8a3a8e938
naveen <185893220+codamee@users.noreply.github.com>
2026-03-13 09:15:31 +0530

    feat:add volume

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ function controls() {
         else if (e.target.id === "previous") {
             const currentSong = spotifyLibrary.findIndex(song => song.url === currentSrc.src)
             if (currentSong + 1 >= 0) {
-                
+
                 playMusic(spotifyLibrary[currentSong - 1].url)
             }
         }
@@ -103,5 +103,31 @@ function controls() {
         document.querySelector(".circle").style.left = percent + "%"
         currentSrc.currentTime = (currentSrc.duration * percent) / 100
     })
+
+    document.querySelector("#volume").addEventListener("input", (e) => {
+        currentSrc.volume = e.target.value / 100
+        const muteIcon = document.querySelector("#no-volume");
+        if (currentSrc.volume === 0) {
+            muteIcon.src = "./assets/mute.svg";
+        } else {
+            muteIcon.src = "./assets/no-volume.svg";
+        }
+    })
+
+    document.querySelector(".sound").addEventListener("click", (e) => {
+        if (e.target.id == "no-volume") {
+            currentSrc.volume = currentSrc.volume == 0 ? 0.5 : 0
+            console.log(e.target);
+            volume.value = currentSrc.volume !== 0 ? 50 : 0
+            e.target.src = currentSrc.volume == 0 ? "./assets/mute.svg" : "./assets/no-volume.svg"
+        }
+        else if (e.target.id == "full-volume") {
+            currentSrc.volume = 1
+            volume.value = 100
+            const muteIcon = document.querySelector("#no-volume");
+            muteIcon.src = "./assets/no-volume.svg"
+        }
+    })
+
 }
 controls()
